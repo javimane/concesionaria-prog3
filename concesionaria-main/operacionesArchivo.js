cc54ab055999d2872776ce675bc93e68698575cf
javier <javiermanesero@gmail.com>
2022-06-22 20:09:22 -0300

    Terminado

---
+++ concesionaria-main/operacionesArchivo.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+
+let operacionesArchivos =
+{
+    archivo: 'autos.json',
+
+    leerArchivoJson: function () {
+
+        return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
+    },
+
+    grabarUnJson: function (autos) {
+        return  fs.writeFileSync(this.archivo,JSON.stringify(autos))
+    },
+
+    
+}
+
+module.exports = operacionesArchivos;
+
